a3a13f78b11d1dce1a5180eb18209ecc0f424b85
wangjie-web <990492493@qq.com>
2017-04-17 14:52:05 +0800

    asd

--- views/home/js/index.js
+++ views/home/js/index.js
@@ -3,12 +3,18 @@ define(['common'], function(com) {
 	com.controller("home_index", ['$scope', '$timeout', function($scope, $timeout) {
 
 		$scope.geolocation = function() {
-			alert(1235)
 			if(navigator.geolocation) {
 				navigator.geolocation.getCurrentPosition(function(e) {
-					alert(JSON.stringify(e))
-				},function(e){
-					console.log(e)
+					var coords = e.coords;
+					var latitude = coords.latitude;
+					var longitude = coords.longitude;
+					alert("lat:"+latitude + "|lng:"+longitude);
+				}, function(e) {
+					alert(e.message + "[code:"+e.code+"]");
+				}, {
+					enableHighAccuracy: false,
+					timeout: 3000,
+					maximumAge: 10 * 1000
 				});
 			} else {
 				alert("Geolocation is not supported by this browser.");
