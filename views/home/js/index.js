913d35636172a28294557b6460a7efe4cad72423
wangjie-web <990492493@qq.com>
2017-04-17 14:16:24 +0800

    add

--- views/home/js/index.js
+++ views/home/js/index.js
@@ -3,10 +3,12 @@ define(['common'], function(com) {
 	com.controller("home_index", ['$scope', '$timeout', function($scope, $timeout) {
 
 		$scope.geolocation = function() {
-			alert(123)
+			alert(1235)
 			if(navigator.geolocation) {
 				navigator.geolocation.getCurrentPosition(function(e) {
 					alert(JSON.stringify(e))
+				},function(e){
+					console.log(e)
 				});
 			} else {
 				alert("Geolocation is not supported by this browser.");
