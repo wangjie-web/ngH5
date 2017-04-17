09fe21443d820b1f374caa42442b0ffb7aeb4f7d
wangjie-web <990492493@qq.com>
2017-04-17 13:44:29 +0800

    sdfsadfsdf

--- views/main/js/index.js
+++ views/main/js/index.js
@@ -1,17 +1,23 @@
 define(['common'], function(com) {
 
 	com.controller("app", ['$scope', '$timeout', function($scope, $timeout) {
-		
-		console.log("index")
+
+		if(navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(e){
+				alert(JSON.stringify(e))
+			});
+		} else {
+			alert("Geolocation is not supported by this browser.");
+		}
 
 		$scope.$on("$viewContentLoaded", function() {
 			//console.log("$viewContentLoaded")
 		});
 
 		$scope.$on("$routeChangeStart", function(event, next, current) {
-			window.scrollTo(0,0);
+			window.scrollTo(0, 0);
 			//console.log("$routeChangeStart");
 		});
-		
+
 	}])
 })
