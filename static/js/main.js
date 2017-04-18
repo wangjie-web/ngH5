5bb37631d1c2c9154c305c33d7ac905327017aec
wangjie-web <990492493@qq.com>
2017-04-18 14:37:33 +0800

    add

--- static/js/main.js
+++ static/js/main.js
@@ -9,7 +9,7 @@ require.config({
 		//"angular-animate":"//cdn.bootcss.com/angular.js/1.3.15/angular-animate.min",
 		'app':"js/app",
 		'common':"js/common",
-		'amap':'//webapi.amap.com/maps?v=1.3&key=608d75903d29ad471362f8c58c550daf'
+		'amap':'//webapi.amap.com/maps?v=1.3&key=8b7a099c7f4364e7f68a9993b770ce94'
 	},
 	shim:{
 		'angular':{
