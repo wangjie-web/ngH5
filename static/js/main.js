2e1d04c99372e81444780c347d79b623f0b2b034
wangjie-web <990492493@qq.com>
2017-04-18 11:35:15 +0800

    add

--- static/js/main.js
+++ static/js/main.js
@@ -9,7 +9,7 @@ require.config({
 		//"angular-animate":"//cdn.bootcss.com/angular.js/1.3.15/angular-animate.min",
 		'app':"js/app",
 		'common':"js/common",
-		'amap':'//webapi.amap.com/maps?v=1.3&key=8b7a099c7f4364e7f68a9993b770ce94'
+		'amap':'//webapi.amap.com/maps?v=1.3&key=8b7a099c7f4364e7f68a9993b770ce94&callback=initAmap'
 	},
 	shim:{
 		'angular':{
@@ -22,22 +22,18 @@ require.config({
 		'angular-animate':{
 			deps:['angular-route'],
 			exports: 'angular-animate'
-		},
-		'amap':{
-			exports:'amap'
 		}
 	},
 	// 防止读取缓存，调试用
 	//urlArgs: "bust=" + (new Date()).getTime(),
 	//更新用户缓存一次
-	urlArgs: "bust=" + 1,
+	urlArgs: "bust=" + 3,
 	waitSeconds: 0 
 });
 define(['angular',
 		'angular-route',
 		//'angular-animate',
-		'app',
-		'amap'
+		'app'
 ],function(angular){
 	angular.bootstrap(document,["app"]);
 });
--- views/home/js/index.js
+++ views/home/js/index.js
@@ -2,55 +2,49 @@ define(['common'], function(com) {
 
 	com.controller("home_index", ['$scope', '$timeout', function($scope, $timeout) {
 
-		if(navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function(e) {
-				var coords = e.coords;
-				var latitude = coords.latitude;
-				var longitude = coords.longitude;
+		window.initAmap = function() {
 
-				var map = new AMap.Map('container', {
-					resizeEnable: true,
-					zoom: 16,
-					center: [longitude, latitude]
-				});
-
-				var marker = new AMap.Marker({
-					icon: 'http://vdata.amap.com/icons/b18/1/2.png', //24px*24px
-					position: [longitude, latitude],
-					offset: new AMap.Pixel(-12, -12),
-					map: map
-				});
-
-				marker = new AMap.Marker({
-					map: map,
-					position: [longitude, latitude]
-				})
-				marker.setLabel({
-					offset: new AMap.Pixel(20, 20), //修改label相对于maker的位置
-					content: "点击打开高德地图"
-				});
-				marker.on('click', function(e) {
-					marker.markOnAMAP({
-						name: '当前位置',
-						position: marker.getPosition()
-					})
-				})
-
-				AMap.plugin(['AMap.ToolBar', 'AMap.Scale'], function() {
-					map.addControl(new AMap.ToolBar());
-					map.addControl(new AMap.Scale());
-				})
-
-			}, function(e) {
-				alert(e.message + "[code:" + e.code + "]");
-			}, {
-				enableHighAccuracy: true,
-				timeout: 3000,
-				maximumAge: 10 * 1000
+			var map, geolocation;
+			//加载地图，调用浏览器定位服务
+			map = new AMap.Map('container', {
+				resizeEnable: true
 			});
-		} else {
-			alert("Geolocation is not supported by this browser.");
+			map.plugin('AMap.Geolocation', function() {
+				geolocation = new AMap.Geolocation({
+					enableHighAccuracy: true, //是否使用高精度定位，默认:true
+					timeout: 10000, //超过10秒后停止定位，默认：无穷大
+					buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+					zoomToAccuracy: true, //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+					buttonPosition: 'RB'
+				});
+				map.addControl(geolocation);
+				geolocation.getCurrentPosition();
+				AMap.event.addListener(geolocation, 'complete', onComplete); //返回定位信息
+				AMap.event.addListener(geolocation, 'error', onError); //返回定位出错信息
+			});
+			//解析定位结果
+			function onComplete(data) {
+				console.log(data);
+				/*
+				var str = ['定位成功'];
+				str.push('经度：' + data.position.getLng());
+				str.push('纬度：' + data.position.getLat());
+				if(data.accuracy) {
+					str.push('精度：' + data.accuracy + ' 米');
+				} //如为IP精确定位结果则没有精度信息
+				str.push('是否经过偏移：' + (data.isConverted ? '是' : '否'));
+				document.getElementById('tip').innerHTML = str.join('<br>');
+				*/
+			}
+			//解析定位错误信息
+			function onError(data) {
+				console.log(data)
+				//document.getElementById('tip').innerHTML = '定位失败';
+			}
+
 		}
+		
+		require(['amap'])
 
 		console.log("home_index")
 
